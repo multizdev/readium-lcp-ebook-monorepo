052ebec53c44593a1f554fbeca1585dba7783784
MultiDevs <multiz.dev@gmail.com>
2024-10-21 17:01:00 +0500

    Add new cover images and remove redundant CSS file
    Added two new cover images to the `publications` directory. Removed the obsolete `readiumAfter.css` file from the `readium` styles module as it is no longer needed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,11 @@
 import React, { ReactElement } from 'react';
 
 import HomePage from '@marketplace/pages/HomePage';
-// import ReadiumReader from '@modules/readium/reader/ReadiumReader';
-import dynamic from 'next/dynamic';
-
-const ReadiumReader = dynamic(
-  () => import('@modules/readium/reader/ReadiumReader'),
-  {
-    ssr: false,
-  },
-);
 
 export default function Home(): ReactElement {
   return (
     <>
-      {/*<HomePage />*/}
-      <ReadiumReader />
+      <HomePage />
     </>
   );
 }
