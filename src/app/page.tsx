ae8be547c36a15652fb87fb0dfddfca681c82fcc
multidevs <multiz.dev@gmail.com>
2024-10-22 18:47:03 +0500

    Delete pnpm-lock.yaml file
    Removed the pnpm-lock.yaml file to eliminate dependency management through pnpm. This change may require reconfiguration of package management setups to ensure all dependencies are correctly handled.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,5 @@ import React, { ReactElement } from 'react';
 import HomePage from '@marketplace/pages/HomePage';
 
 export default function Home(): ReactElement {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+  return <HomePage />;
 }
