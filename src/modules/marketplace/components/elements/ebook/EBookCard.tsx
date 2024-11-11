d5f2d701b9ee87fb9edc3ef49e0a56e3827d50ac
multidevs <multiz.dev@gmail.com>
2024-11-11 23:20:26 +0500

    Config update

--- src/modules/marketplace/components/elements/ebook/EBookCard.tsx
+++ src/modules/marketplace/components/elements/ebook/EBookCard.tsx
@@ -53,6 +53,7 @@ function EBookCard({ book }: { book: metadata }): ReactElement {
   return (
     <Card key={id} className="flex flex-col">
       <CardHeader>
+        <div>${process.env.NEXT_PUBLIC_IMAGE_HOST}</div>
         <div className="aspect-[3/4] relative mb-4">
           <Image
             src={`${process.env.NEXT_PUBLIC_IMAGE_HOST}/publications/cover-images/${content_id}.png`}
